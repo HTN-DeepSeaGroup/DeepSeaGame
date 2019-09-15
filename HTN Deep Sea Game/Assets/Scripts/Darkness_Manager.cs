d6cb4dac55d53ee283e7b9d2e7ade930b125d76b
Spacepuffin <Spacepuffin7@gmail.com>
2019-09-15 00:19:45 -0400

    Darkness Vignette

---
+++ HTN Deep Sea Game/Assets/Scripts/Darkness_Manager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Darkness_Manager : MonoBehaviour
+{
+    public float darkness;
+    public float[] opacities; 
+    public SpriteRenderer[] vignettes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        opacities = new float[3];
+        vignettes = new SpriteRenderer[3];
+        darkness = 0;
+        vignettes = GetComponentsInChildren<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        print(opacities);
+        opacities[0] = Mathf.Max(0, Mathf.Min(darkness / 1200, 1));
+        opacities[1] = Mathf.Max(0, Mathf.Min((darkness-1200) / 1200, 1));
+        opacities[2] = Mathf.Max(0, Mathf.Min((darkness-2400) / 1200, 1));
+        for (int i = 0; i<3; i++)
+        {
+            vignettes[i].color = new Color(1f, 1f, 1f, opacities[i]);
+        } 
+        
+        darkness++; 
+    }
+}
