3e4752b48ce77d688cc7a5d49f89ccd53e3b79c2
Spacepuffin <Spacepuffin7@gmail.com>
2019-09-14 21:20:01 -0400

    basic movement

---
+++ HTN Deep Sea Game/Assets/Scripts/Controls.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    float horizontalMovement, verticalMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponentInChildren<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = Input.GetAxisRaw("Vertical");
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        print(horizontalMovement);
+        rb.velocity= new Vector2(horizontalMovement*2, rb.velocity[1]);
+    }
+}
