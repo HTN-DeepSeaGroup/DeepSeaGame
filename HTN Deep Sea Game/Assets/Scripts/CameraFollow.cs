4552521f9a7f41c9cf04aa3ccdb7533ea80e340e
Spacepuffin <Spacepuffin7@gmail.com>
2019-09-15 00:42:25 -0400

    Camera Fix

--- HTN Deep Sea Game/Assets/Scripts/CameraFollow.cs
+++ HTN Deep Sea Game/Assets/Scripts/CameraFollow.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class CameraFollow : MonoBehaviour
 {
-    public Transform player;
+    [SerializeField] public Transform player;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +15,6 @@ public class Camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, 0); // Camera follows the player with specified offset position
+        transform.position = new Vector3(player.position.x, player.position.y, -10); // Camera follows the player with specified offset position
     }
 }
